6aaf54a0006a9d6215c6cae35c58fb1786a79e6c
Erik Tollefsrud <erik.tollefsrud@breckschool.org>
2025-10-26 15:32:02 -0500

    change obsidian plugin to offical

--- nvim/dot-config/nvim/lua/plugins/obsidian.lua
+++ nvim/dot-config/nvim/lua/plugins/obsidian.lua
@@ -1,21 +1,24 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",
+  "obsidian-nvim/obsidian.nvim", -- https://github.com/obsidian-nvim/obsidian.nvim
+  version = "*", -- recommended, use latest release instead of latest commit
   lazy = false,
   ft = "markdown",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
+  -- Replace the above line with this if you want to load obsidian.nvim for markdown files in your vault:
+  -- event {
+  --  -- If you want to use shortcut '~' here you need to call 'vim.fn.expand'.
+  --  -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-fault/*.md"
+  --  -- refer to `:h file-pattern` for more examples.
+  --  "BufReadPre path/to/my-vault/*.md",
+  --  "BufReadFile path/to/my-vault/*md"
+  -- },
+  ---@module 'obsidian'
+  ---@type obsidian.config
   opts = {
     workspaces = {
       {
-        name = "Work",
+        name = "work",
         path = "~/Documents/ObsidianVault",
       },
-      -- {
-      --   name = "Coding Notes",
-      --   path = "~/Documents/Coding Notes",
-      -- },
     },
   },
 }
